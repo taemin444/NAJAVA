ebafb9cbbdc5c85fee8d5a51e095a3d86110f13a
taemin444 <fresh1115@naver.com>
2024-01-30 15:59:29 +0900

    rootContext

--- src/main/java/com/my/config/RootContext.java
+++ src/main/java/com/my/config/RootContext.java
@@ -3,7 +3,7 @@ package com.my.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
