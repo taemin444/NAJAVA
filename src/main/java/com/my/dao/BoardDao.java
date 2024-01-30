e00a20ae1e0863280f05b8367b0aaf5e304f9656
bada <silverzoo030@gmail.com>
2024-01-30 16:49:04 +0900

    commit for board things

--- src/main/java/com/my/dao/BoardDao.java
+++ src/main/java/com/my/dao/BoardDao.java
@@ -1,2 +1,30 @@
-package com.my.dao;public class BoardDao {
+package com.my.dao;
+
+import com.my.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface BoardDao {
+    BoardDto select(Integer bno) throws Exception;
+
+    List<BoardDto> selectAll() throws Exception;
+
+    int count() throws Exception;
+
+    int delete(Integer bno, String writer) throws Exception;
+
+    int deleteAll() throws Exception;
+
+    int insert(BoardDto dto) throws Exception;
+
+    int update(BoardDto dto) throws Exception;
+
+    int updateCommentCnt(Integer bno, Integer cnt) throws Exception;
+
+    int increaseViewCnt(Integer bno) throws Exception;
+
+    List<BoardDto> selectPage(Map map) throws Exception;
 }
--- src/main/java/com/my/dto/BoardDto.java
+++ src/main/java/com/my/dto/BoardDto.java
@@ -1,2 +1,24 @@
-package com.my.dto;public class BoardDto {
+package com.my.dto;
+
+import lombok.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDto {
+    private Integer bno;
+    private String title;
+    private String content;
+    private String writer;
+    private int view_cnt;
+    private int comment_cnt;
+    @EqualsAndHashCode.Exclude
+    private Date reg_date;
+
+    public BoardDto(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
 }
